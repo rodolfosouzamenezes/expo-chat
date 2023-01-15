e250248f8f17ee0c3f71a265c19ba8c91294e59e
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-15 20:17:32 -0300

    feat: contacts listed by name

--- src/screens/Contacts.tsx
+++ src/screens/Contacts.tsx
@@ -4,33 +4,52 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
 
 import { useAppSelector } from "../store";
-import { get, getDatabase, ref } from "firebase/database";
+import { get, getDatabase, ref, orderByChild, query, orderByValue } from "firebase/database";
 import { firebase } from "../config/firebase";
 import { IContact, setContacts } from "../features/chat.slice";
 import { ContactItem } from "../components/ContactItem";
 
 export function Contacts() {
   const { contacts } = useAppSelector((state) => state.chat)
+  const { uid } = useAppSelector((state) => state.auth.user)
   const navigation = useNavigation();
   const database = getDatabase(firebase);
   const dispatch = useDispatch();
 
   const getContacts = async () => {
     let contacts: IContact[] = [];
-    await get(ref(database, 'users/'))
+
+    const que = query(ref(database, 'users'), orderByChild('name'))
+
+    await get(que)
       .then((usersResponse) => {
         usersResponse.forEach((user) => {
           const name = user.val().name as string;
           const id = user.key;
-          contacts.push({ name, id })          
+
+          id !== uid &&
+            contacts.push({ name, id })
         })
       });
+
+    contacts.sort((a, b) => {
+      var nameA = a.name.toUpperCase(); 
+      var nameB = b.name.toUpperCase(); 
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0;
+    })
 
     dispatch(setContacts(contacts))
   }
 
   const handleContactPress = (id: string) => {
-    console.log(id);   
+    console.log(id);
   }
 
   useFocusEffect(useCallback(() => {
@@ -41,8 +60,8 @@ export function Contacts() {
     <View style={{ flex: 1, }}>
       <FlatList
         data={contacts}
-        renderItem={({item}) => {
-          return <ContactItem data={item} onPress={handleContactPress}/>
+        renderItem={({ item }) => {
+          return <ContactItem data={item} onPress={handleContactPress} />
         }}
       />
     </View>
