d0b6ad4991dd64885a2ac6f0d6d19b00d0b12a74
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-25 18:07:11 -0300

    feat: environment variables configured

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config'
+
+export default {
   "expo": {
     "name": "Chat",
     "slug": "expo-chat",
@@ -23,7 +25,15 @@
     "extra": {
       "eas": {
         "projectId": "f3097a56-0c0d-4c9d-ad84-c22896945f97"
-      }
+      },
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
     }
   }
 }
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,33 +1,40 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getReactNativePersistence } from 'firebase/auth/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
+import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import {
   initializeAuth,
-  getAuth,
   updateProfile,
   createUserWithEmailAndPassword,
-  onAuthStateChanged,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
+  getAuth
 } from 'firebase/auth';
 
+// Initialize Firebase configuration using information from .env file
 const firebaseConfig = {
-  // your config here
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  databaseURL: Constants.manifest.extra.databaseURL,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  measurementId: Constants.manifest.extra.measurementId,
 };
-
 const firebase = initializeApp(firebaseConfig);
 
-// Asynchronous Data Storage Persistence
+// Asynchronous Auth Data Storage Persistence
 initializeAuth(firebase, { persistence: getReactNativePersistence(AsyncStorage) });
+export const auth = getAuth() 
 
-const auth = getAuth()
+// Firestore Database
+export const db = getFirestore(firebase)
 
 export {
-  firebase,
-  auth,
   createUserWithEmailAndPassword,
   updateProfile,
-  onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut
 }
